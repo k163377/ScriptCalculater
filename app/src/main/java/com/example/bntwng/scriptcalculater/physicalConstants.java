f367046dfd1d0d9d20d3172e88a2e913306d59c5
k163377 <k163377@edu.tut.ac.jp>
2017-03-12 12:08:25 +0900

    なんかおかしいので上げ直し

---
+++ app/src/main/java/com/example/bntwng/scriptcalculater/physicalConstants.java
@@ -0,0 +1,18 @@
+package com.example.bntwng.scriptcalculater;
+
+/**
+ * Created by bntwng on 2017/03/12.
+ */
+
+public class physicalConstants {
+    public static final double c = 299792458;//光速
+    public static final double el = 1.6021766208*Math.pow(10,-19);//電気素量
+    public static final double f = 96485.33289;//ファラデー定数
+    public static final double g = 6.6708/100000000000.0;//重力定数
+    public static final double h = 6.626070040*Math.pow(10,-34);//プランク定数
+    public static final double k = 1.38064852*Math.pow(10,-23);//ボルツマン定数
+    public static final double l = 6.022140857*Math.pow(10,23);//アボガドロ定数
+    public static final double r = 8.3144598;//モル気体定数
+
+    private physicalConstants(){}//コンストラクタはプライベート
+}
