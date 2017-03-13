1f748cba3944c7ba746a9dc9cbb7fe6c004d3073
k163377 <k163377@edu.tut.ac.jp>
2017-03-13 19:19:32 +0900

    min, max, powを実装

--- app/src/main/java/com/example/bntwng/scriptcalculator/functions/FunctionNode.java
+++ app/src/main/java/com/example/bntwng/scriptcalculator/functions/FunctionNode.java
@@ -2,9 +2,7 @@ package com.example.bntwng.scriptcalculator.functions;
 
 import com.example.bntwng.scriptcalculator.Node;
 
-/**
- * Created by bntwng on 2017/03/12.
- */
+//log, max, min, powは使えるようにしたことをwikiに書いていない
 
 public class FunctionNode extends Node {
     public enum functions3{
@@ -15,13 +13,31 @@ public class FunctionNode extends Node {
         CSC,
         SEC,
         COT,
-        //log
+        //基礎的な関数
         LOG,
-        //abs
+        POW,
+        //その他関数
         ABS,
-        //exp
         EXP,
+        MAX,
+        MIN,
     }
+    public enum functions4{//まだ完全にやってない
+        //逆三角関数
+        ASIN,
+        ACOS,
+        ATAN,
+        //双曲線関数
+        SINH,
+        COSH,
+        TANH,
+        CSCH,
+        SECH,
+        COTH,
+        //ルート
+        ROOT,
+    }
+
     protected double logCalculate(String formula) throws IllegalArgumentException{
         try {
             double d1 = calculate(formula);//とりあえず消しきれなかったときはエラーを吐いておく、後で,区切りに関して実装
@@ -31,6 +47,47 @@ public class FunctionNode extends Node {
                 return Math.log10(d1) / Math.log10(d2);
             }
             return Math.log(d1);
+        }catch (Exception e){
+            throw e;
+        }
+    }
+    protected double powCalculate(String formula)throws IllegalArgumentException{
+        try{
+            double d1 = calculate(formula);
+            if (right >= 0) {
+                double d2 = calculate(formula.substring(0,right));//カンマ後はsubstringを取り、自動でrightが減るため、ここでrightを触る必要は無い
+                if(right>=0)throw new IllegalArgumentException("pow関数内に文法エラー");
+                return Math.pow(d2,d1);
+            }else{
+                throw new IllegalArgumentException("pow関数への入力が少ないです");
+            }
+
+        }catch (Exception e){
+            throw e;
+        }
+    }
+    protected double maxCalculate(String formula) throws IllegalArgumentException {
+        try {
+            if(formula.charAt(0)==',')throw new IllegalArgumentException("max関数の先頭に,");
+            double d1 = calculate(formula);
+            double d2;
+            while (right>=0){
+                if(d1 < (d2 = calculate(formula)))d1 = d2;
+            }
+            return d1;
+        }catch (Exception e){
+            throw e;
+        }
+    }
+    protected double minCalculate(String formula) throws IllegalArgumentException {
+        try {
+            if(formula.charAt(0)==',')throw new IllegalArgumentException("max関数の先頭に,");
+            double d1 = calculate(formula);
+            double d2;
+            while (right>=0){
+                if(d1 > (d2 = calculate(formula)))d1 = d2;
+            }
+            return d1;
         }catch (Exception e){
             throw e;
         }
@@ -62,14 +119,32 @@ public class FunctionNode extends Node {
                 case LOG:
                     value = logCalculate(formula);
                     break;
-                //abs
+                //pow
+                case POW:
+                    value = powCalculate(formula);
+                    break;
+                //その他関数
                 case ABS:
                     value = Math.abs(calculate(formula));
                     break;
                 case EXP:
                     value = Math.exp(calculate(formula));
+                    break;
+                case MAX:
+                    value = maxCalculate(formula);
+                    break;
+                case MIN:
+                    value = maxCalculate(formula);
+                    break;
             }
         }catch (Exception e){
+            throw e;
+        }
+    }
+    public FunctionNode(String formula,functions4 f) throws IllegalArgumentException {
+        try {
+
+        } catch (Exception e) {
             throw e;
         }
     }
