de0d9264a7699256ecdccd1fcc7a37390ad21b8a
k163377 <k163377@edu.tut.ac.jp>
2017-03-17 20:02:50 +0900

    凄く雑だけどメニューから使い方を見られるように変更

--- app/src/main/java/com/example/bntwng/scriptcalculator/MainActivity.java
+++ app/src/main/java/com/example/bntwng/scriptcalculator/MainActivity.java
@@ -1,15 +1,23 @@
-package com.example.bntwng.scriptcalculater;
+package com.example.bntwng.scriptcalculator;
 
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,10 +26,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.net.URLDecoder;
 
 public class MainActivity extends AppCompatActivity {
     static String ln = System.getProperty("line.separator");
+
+    boolean webViewIsEnable = false;
 
     Button doButton;
     EditText inputFormula;
@@ -30,15 +42,39 @@ public class MainActivity extends AppCompatActivity {
 
     InputMethodManager inputMethodManager;
 
+    AdView mAdView;
+
+    WebView wv;
+
     protected void backUp(){//メモを保存
         try {
-            File file = new File(getFilesDir().getPath() + "/temp");
+            //File file = new File(getFilesDir().getPath() + "/temp");
+            File file = new File(getFilesDir(),"temp");
             PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
 
             pw.println(memoEditor.getText());
             pw.close();
         }catch (IOException e){
             messageView.setText("バックアップに失敗しました、念のためデータの外部への保存を推奨します");
+        }
+    }
+    protected void initMemoEditor(){
+        try{//バックアップからの復元
+            File file = new File(getFilesDir().getPath() + "/temp");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            StringBuilder sb = new StringBuilder(br.readLine());//null突っ込んでもsbがExceotion吐くのでちゃんと終わる
+            String str = br.readLine();
+
+            while(str!=null){
+                sb.append(str);
+                sb.append(ln);
+                str = br.readLine();
+            }
+            memoEditor.setText(sb.toString());
+            br.close();
+        }catch (Exception e){//ファイルが無いとか読み込みミスったら何もしない
+            //messageView.setText("onCreate:" + e.getMessage());//念のための出力、今はコメントアウト
         }
     }
 
@@ -74,23 +110,7 @@ public class MainActivity extends AppCompatActivity {
         messageView = (TextView)  findViewById(R.id.messageView);
 
         memoEditor = (EditText) findViewById(R.id.memoEditor);
-        try{//バックアップからの復元
-            File file = new File(getFilesDir().getPath() + "/temp");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-
-            StringBuilder sb = new StringBuilder(br.readLine());//null突っ込んでもsbがExceotion吐くのでちゃんと終わる
-            String str = br.readLine();
-
-            while(str!=null){
-                sb.append(str);
-                sb.append(ln);
-                str = br.readLine();
-            }
-            memoEditor.setText(sb.toString());
-            br.close();
-        }catch (Exception e){//ファイルが無いとか読み込みミスったら何もしない
-            //messageView.setText("onCreate:" + e.getMessage());//念のための出力、今はコメントアウト
-        }
+        initMemoEditor();//バックアップからの復元
 
         inputFormula = (EditText) findViewById(R.id.inputFormula);
         inputFormula.setOnKeyListener(new View.OnKeyListener() {
@@ -117,44 +137,69 @@ public class MainActivity extends AppCompatActivity {
                 calculate();
             }
         });
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-8906600258681229~8490126796");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        //wv = (WebView)findViewById(R.id.webView);//webView初期化
+        /*wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                return false;
+            }
+        });*/
     }
 
     @Override
     protected void onDestroy(){
         backUp();
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
         super.onDestroy();
     }
 
-    /*@Override//メニュー関連
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        try {
-            if (requestCode == CHOSE_FILE_CODE && resultCode == RESULT_OK) {
-                String filePath = data.getDataString().replace("file://", "");
-                String decodedfilePath = URLDecoder.decode(filePath, "utf-8");
-                //imageText.setText(decodedfilePath);
-            }
-        } catch (UnsupportedEncodingException e) {
-            // いい感じに例外処理
-        }
-    }*/
-    /*@Override//メニュー関連
+    @Override//メニュー関連
     public boolean onCreateOptionsMenu(Menu menu) {//メニューの表示
         getMenuInflater().inflate(R.menu.option_menu, menu);
         return true;
-    }*/
+    }
 
-    /*@Override
+    @Override
     public boolean onOptionsItemSelected(MenuItem item){//メニューが選ばれた時の対応
         switch(item.getItemId()){
-            case R.id.option_menu_item0://保存
-                //saveMemo();
+            case R.id.option_menu_item0://定数
+                setContentView(R.layout.web_view);
+                webViewIsEnable = true;
+                wv = (WebView)findViewById(R.id.webView);//webView初期化
+                wv.loadUrl("https://github.com/k163377/ScriptCalculater/wiki/%E4%BD%BF%E7%94%A8%E5%8F%AF%E8%83%BD%E3%81%AA%E5%AE%9A%E6%95%B0");
                 break;
-            case R.id.option_menu_item1://読み込み
+            case R.id.option_menu_item1://演算子
+                setContentView(R.layout.web_view);
+                webViewIsEnable = true;
+                wv = (WebView)findViewById(R.id.webView);//webView初期化
+                wv.loadUrl("https://github.com/k163377/ScriptCalculater/wiki/%E4%BD%BF%E7%94%A8%E5%8F%AF%E8%83%BD%E3%81%AA%E6%BC%94%E7%AE%97%E5%AD%90");
                 break;
-            case R.id.option_menu_item2://ヘルプ？増やす予定で
+            case R.id.option_menu_item2://関数
+                setContentView(R.layout.web_view);
+                webViewIsEnable = true;
+                wv = (WebView)findViewById(R.id.webView);//webView初期化
+                wv.loadUrl("https://github.com/k163377/ScriptCalculater/wiki/%E4%BD%BF%E7%94%A8%E5%8F%AF%E8%83%BD%E3%81%AA%E9%96%A2%E6%95%B0");
                 break;
         }
         return true;
-    }*/
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode,KeyEvent event){//webViewを起動したときの戻るキーの挙動制御
+        if(keyCode==KeyEvent.KEYCODE_BACK && webViewIsEnable){
+            setContentView(R.layout.activity_main);
+            webViewIsEnable = false;
+            return true;
+        }
+        return false;
+    }
 }
 
